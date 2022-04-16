91da0d2b30708740b01b5c1b47059384103baa35
Alessio <alessio26manfredini@gmail.com>
2022-04-16 19:33:52 +0200

    Try different background

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import displayHome from "./home.js";
 import './style.css';
+import background from "./background-site.png";
 
+let body = document.querySelector("body");
+body.style.cssText = `background-image: url("${background}");
+background-size: 100%`
 displayHome();
