2a18f85ab72365a296b3a93a662cc9081ad95fb4
Alessio <alessio26manfredini@gmail.com>
2022-04-16 19:49:01 +0200

    Adjust home styling

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import background from "./background-site.png";
 
 let body = document.querySelector("body");
 body.style.cssText = `background-image: url("${background}");
-background-size: 100%`
+background-size: 100%;
+background-attachment: fixed;`
 displayHome();
