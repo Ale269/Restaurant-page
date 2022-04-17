c35fc69febdc52a6c363e09d668bac20af99bcc9
Alessio <alessio26manfredini@gmail.com>
2022-04-17 20:29:46 +0200

    Add no repeat to background image

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import displayMenu from "./menu.js";
 import './style.css';
 import background from "./background-site.png";
 
-let body = document.querySelector("body");
-body.style.cssText = `background-image: url("${background}");
+let externalContainer = document.querySelector(".image-background");
+externalContainer.style.cssText = `background-image: url("${background}");
 background-size: 100%;
-background-attachment: fixed;`
+background-attachment: fixed;
+background-repeat: no-repeat;`
 
 
 displayHome();
